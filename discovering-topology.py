00879b0bc15750b9188b755bae18c1a631c077b9
awils415 <73905468+awils415@users.noreply.github.com>
2023-10-01 18:17:57 -0400

    Create discovering-topology.py
    Python script that automates the traceroute to various IP addresses

---
+++ discovering-topology.py
@@ -0,0 +1,18 @@
+import scapy.all
+from scapy.all import IP, UDP, sr1
+
+ipaddr = "10.0.0.0"
+for i in range(1, 28):
+  pkt = IP(dst=ipaddr, ttl=i) / UDP(dport=33434)
+  # Send the packet and get a reply
+  reply = sr1(pkt, verbose=0)
+  if reply is None:
+    # No reply
+    break
+  elif reply.type == 3:
+    # We've reached our destination
+    print("Done!", reply.src)
+    break
+  else:
+     # We're in the middle somewhere
+     print("%d hops away: " % i , reply.src)
